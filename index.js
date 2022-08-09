fb8115a763f1c3518a61a3a2e427dc5ac6be0316
Luiz Ricardo <64525544+RickLuiz@users.noreply.github.com>
2022-08-09 05:46:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-
 const app = express()
 
 app.use(morgan('dev'))
